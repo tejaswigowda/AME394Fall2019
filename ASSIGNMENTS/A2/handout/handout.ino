a33594528c7965fb739250e0734da250106bbad4
tejaswigowda <tejaswi@foxyninjastudios.com>
2019-09-25 09:35:28 -0700

    in class

---
+++ ASSIGNMENTS/A2/handout/handout.ino
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+
+char * HOSTNAME = "test12345";
+char * WifiPASS = "";
+
+#include <WiFi.h>
+#include <WebServer.h>
+
+
+
+WebServer server(80);
+
+
+void setup() {
+    Serial.begin(115200);
+
+    
+
+    // Start Wifi AP
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAP(HOSTNAME, WifiPASS);
+
+
+      
+    // handle index -- HTTP Server
+
+    
+    server.on("/", []() {
+      digitalWrite(21, 1);
+      String v = server.arg("v");
+      
+      Serial.println(v);
+      // write `v` String to Screen
+
+      server.send(200, "text/html", "<html><head><script>function foo(){var v = document.getElementById('theText').value;window.location.href=\"./?v=\" + v}</script></head><body><input type='text' maxlength='100' id='theText'><button  onclick='foo()'  > Submit </button></body><script>document.getElementById(\"theText\").value=window.location.search.replace(\"?v=\",\"\")</script><html>");
+    });
+
+    
+    server.begin();
+    
+}
+
+void loop() {
+    server.handleClient();  
+}
