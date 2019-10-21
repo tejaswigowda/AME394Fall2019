0102fa5985110d9ec348632c52a682865738b77d
tejaswigowda <tejaswi@foxyninjastudios.com>
2019-10-21 09:54:32 -0700

    in class

--- watchTutorials/DHT11Default/DHT11Default.ino
+++ watchTutorials/DHT11Default/DHT11Default.ino
@@ -1,14 +1,54 @@
 #include <SimpleDHT.h>
 
+#include <WiFi.h>
+#include <HTTPClient.h>
+ 
+const char* ssid = "yourNetworkName";
+const char* password =  "yourPassword";
+
+ 
 // for DHT11, 
 //      VCC: 5V or 3V
 //      GND: GND
-//      DATA: 2
+//      DATA: 21
 int pinDHT11 = 21;
 SimpleDHT11 dht11(pinDHT11);
 
+
+void sendData(float t, float h)
+{
+  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+ 
+    HTTPClient http;
+ 
+    http.begin("http://34.216.181.26:3000/setValue?t=" + String(t) + "&h=" + String(h)); //Specify the URL
+    int httpCode = http.GET();                                        //Make the request
+ 
+    if (httpCode > 0) { //Check for the returning code
+ 
+        String payload = http.getString();
+        Serial.println(httpCode);
+        Serial.println(payload);
+      }
+ 
+    else {
+      Serial.println("Error on HTTP request");
+    }
+ 
+    http.end(); //Free the resources
+  }
+}
+
 void setup() {
   Serial.begin(115200);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
 }
 
 void loop() {
@@ -28,7 +68,7 @@ void loop() {
   Serial.print("Sample OK: ");
   Serial.print((int)temperature); Serial.print(" *C, "); 
   Serial.print((int)humidity); Serial.println(" H");
-  
+  sendData((float)temperature, (float)humidity);
   // DHT11 sampling rate is 1HZ.
   delay(1500);
 }
